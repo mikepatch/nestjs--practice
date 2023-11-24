97bc91e08410565fb798549a80abbf9d3984a263
Michal Lata <michallata.dev@gmail.com>
2023-11-24 13:54:14 +0100

    feat: Add 'products' endpoint with methods

---
+++ src/products/dto/new-product.dto.ts
@@ -0,0 +1,32 @@
+import {
+  IsCurrency,
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  IsUrl,
+} from 'class-validator';
+
+export class NewProductDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsNumber({ maxDecimalPlaces: 2 })
+  price: number;
+
+  @IsUrl({ require_protocol: true })
+  image: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsNumber()
+  @IsInt()
+  @IsOptional()
+  @IsPositive()
+  stock?: number;
+}
