313226c066cdd995d6aef5f255e8348269c3ccaa
Michal Lata <michallata.dev@gmail.com>
2023-11-29 13:07:30 +0100

    patch: refactor products service

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -3,38 +3,52 @@ import { productList } from './product-list';
 import { IProduct } from './product.interface';
 import { NewProductDto } from './dto/new-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { CategoriesService } from '../categories/categories.service';
 
 @Injectable()
 export class ProductsService {
   private productId: number = productList.length;
   private products: IProduct[] = productList;
 
+  constructor(private categoriesService: CategoriesService) {}
+
   private findProduct(id: number): IProduct {
     const product = this.products.find((p) => p.id === id);
     if (!product) {
       throw new NotFoundException(`Product with id: ${id} was not found`);
     }
+
     return product;
   }
 
-  createNew(product: NewProductDto): IProduct {
-    const newProduct: IProduct = { id: ++this.productId, stock: 0, ...product };
-    this.products.push(newProduct);
+  private generateNextId() {
+    return Math.max(...this.products.map((product) => product.id)) + 1;
+  }
 
+  createNew(product: NewProductDto): IProduct {
+    this.categoriesService.getOneById(product.categoryId);
+    const newProduct: IProduct = {
+      id: this.generateNextId(),
+      stock: 0,
+      ...product,
+    };
+
+    this.products.push(newProduct);
     return newProduct;
   }
 
-  getAll(name: string = ''): IProduct[] {
+  getAll(name: string = ''): readonly IProduct[] {
     return this.products.filter((product) =>
       product.name.toLowerCase().includes(name.toLowerCase()),
     );
   }
 
-  getOneById(id: number) {
+  getOneById(id: number): IProduct {
     return this.findProduct(id);
   }
 
-  update(id: number, partialProduct: UpdateProductDto) {
+  update(id: number, partialProduct: UpdateProductDto): IProduct {
+    this.categoriesService.getOneById(partialProduct.categoryId);
     const productToUpdate = this.findProduct(id);
     Object.assign(productToUpdate, partialProduct);
 
