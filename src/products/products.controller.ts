6fb789e0f8bced4351f9dab8159bd6e2c7644784
Michal Lata <michallata.dev@gmail.com>
2023-11-29 20:23:38 +0100

    patch: add parsing pipes and api-key guard to products controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   Body,
   Controller,
   Delete,
@@ -7,15 +8,22 @@ import {
   HttpStatus,
   Logger,
   Param,
+  ParseIntPipe,
   Patch,
   Post,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import * as fsp from 'node:fs/promises';
 import { IProduct } from './product.interface';
 import { NewProductDto } from './dto/new-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { ProductsService } from './products.service';
+import {
+  AcceptableLanguages,
+  ClientLanguage,
+} from '../middlewares/client-language.decorator';
+import { ApiKeyGuard } from '../guards/api-key.guard';
 
 @Controller('products')
 export class ProductsController {
@@ -24,6 +32,7 @@ export class ProductsController {
   constructor(private productsService: ProductsService) {}
 
   @Post()
+  @UseGuards(ApiKeyGuard)
   addNew(@Body() product: NewProductDto): IProduct {
     this.logger.log('About to add');
     this.logger.log(product);
@@ -55,14 +64,23 @@ export class ProductsController {
     return { fileData };
   }
 
+  @Get('sample-error')
+  async getSampleError(@ClientLanguage() lang: AcceptableLanguages) {
+    throw new BadRequestException(
+      lang === 'pl'
+        ? 'Błąd z przykładową wiadomością'
+        : 'Error with sample message',
+    );
+  }
+
   @Get(':productId')
-  getOne(@Param('productId') productId: number): IProduct {
+  getOne(@Param('productId', ParseIntPipe) productId: number): IProduct {
     return this.productsService.getOneById(productId);
   }
 
   @Patch(':productId')
   update(
-    @Param('productId') productId: number,
+    @Param('productId', ParseIntPipe) productId: number,
     @Body() product: UpdateProductDto,
   ): IProduct {
     return this.productsService.update(productId, product);
@@ -70,7 +88,7 @@ export class ProductsController {
 
   @Delete(':productId')
   @HttpCode(HttpStatus.NO_CONTENT)
-  remove(@Param('productId') productId: number) {
+  remove(@Param('productId', ParseIntPipe) productId: number) {
     return this.productsService.removeById(productId);
   }
 }
