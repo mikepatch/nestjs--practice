5d7ca81d0bb59db724eb12410b01444fd8777e7d
Michal Lata <michallata.dev@gmail.com>
2023-11-29 19:39:48 +0100

    patch: add logger to all-errors filter

--- src/errors/all-errors.filter.ts
+++ src/errors/all-errors.filter.ts
@@ -4,14 +4,20 @@ import {
   ExceptionFilter,
   HttpException,
   HttpStatus,
+  Logger,
 } from '@nestjs/common';
 import { Response } from 'express';
 
 @Catch(Error)
 export class AllErrorsFilter implements ExceptionFilter {
+  private logger = new Logger(AllErrorsFilter.name);
+
   catch(exception: any, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+
+    this.logger.debug(`Client preferred language is ${request['language']}`);
 
     if (exception instanceof HttpException) {
       response.status(exception.getStatus()).json(exception.getResponse());
