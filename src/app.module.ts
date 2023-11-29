34c3497295be6330c3c23c10876f813aa356cfb4
Michal Lata <michallata.dev@gmail.com>
2023-11-29 13:11:29 +0100

    feat: add pino to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { LoggerModule } from 'nestjs-pino';
+
 import { CategoriesController } from './categories/categories.controller';
 import { ProductsController } from './products/products.controller';
 import { ProductsService } from './products/products.service';
 import { CategoriesService } from './categories/categories.service';
 
 @Module({
-  imports: [],
+  imports: [LoggerModule.forRoot()],
   controllers: [CategoriesController, ProductsController],
   providers: [ProductsService, CategoriesService],
 })
