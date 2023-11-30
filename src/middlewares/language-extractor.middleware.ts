90982147a3d1aa93db9d807199e7d1c47993f805
Michal Lata <michallata.dev@gmail.com>
2023-11-30 16:20:59 +0100

    patch: refactor language-extractor middleware

--- src/middlewares/language-extractor.middleware.ts
+++ src/middlewares/language-extractor.middleware.ts
@@ -1,10 +1,13 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
+import acceptLanguage from 'accept-language';
+
+acceptLanguage.languages(['pl-PL', 'en-US']);
 
 @Injectable()
 export class LanguageExtractorMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
-    req['language'] = req.headers['accept-language'] || 'en';
-
+    const acceptLanguageHeader = req.headers['accept-language'] || '';
+    req['language'] = acceptLanguage.get(acceptLanguageHeader);
     next();
   }
 }
