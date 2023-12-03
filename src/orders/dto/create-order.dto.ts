09232ce41544dda42c63ebf82c39257153a69c53
Michal Lata <michallata.dev@gmail.com>
2023-12-03 16:01:20 +0100

    feat: add CreateOrderDto class

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,1 +1,17 @@
-export class CreateOrderDto {}
+import { IsArray, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { IProductInOrder } from '../model/product-in-order.interface';
+import { TStatus } from '../model/order.interface';
+
+export class CreateOrderDto {
+  @IsArray()
+  @IsNotEmpty()
+  products: IProductInOrder[];
+
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsOptional()
+  status?: TStatus;
+}
