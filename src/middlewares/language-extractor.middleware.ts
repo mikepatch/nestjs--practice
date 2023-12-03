d73aff3c5ce302048299739e82a7cc7eecc48a05
Michal Lata <michallata.dev@gmail.com>
2023-12-03 15:58:29 +0100

    feat: add language service as a shared module

--- src/middlewares/language-extractor.middleware.ts
+++ src/middlewares/language-extractor.middleware.ts
@@ -1,13 +1,19 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import acceptLanguage from 'accept-language';
+import { LanguageService } from '../shared/language/language.service';
 
-acceptLanguage.languages(['pl-PL', 'en-US']);
+// acceptLanguage.languages(['en-US', 'pl-PL']);
 
 @Injectable()
 export class LanguageExtractorMiddleware implements NestMiddleware {
+  constructor(languageService: LanguageService) {
+    acceptLanguage.languages(
+      languageService.supportedLanguages() as unknown as string[],
+    );
+  }
+
   use(req: any, res: any, next: () => void) {
-    const acceptLanguageHeader = req.headers['accept-language'] || '';
-    req['language'] = acceptLanguage.get(acceptLanguageHeader);
+    req['language'] = acceptLanguage.get(req.headers['accept-language']);
     next();
   }
 }
