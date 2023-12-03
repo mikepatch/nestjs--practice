26b1c399fcdc0a67e85645f5f17bb94e7c9cec9f
Michal Lata <michallata.dev@gmail.com>
2023-12-03 16:00:39 +0100

    feat: add orders service methods

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,29 +1,105 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
 import { ProductsService } from '../product/products/products.service';
+import { IOrder } from './model/order.interface';
+import { IProductInOrder } from './model/product-in-order.interface';
 
 @Injectable()
 export class OrdersService {
+  private orders: IOrder[] = [
+    {
+      id: 1,
+      title: 'test',
+      madeAt: new Date(),
+      status: 'OPENED',
+      products: [{ id: 1, productId: 2, quantity: 5 }],
+      totalPrice: 200,
+    },
+  ];
+
   constructor(private productsService: ProductsService) {}
 
-  create(createOrderDto: CreateOrderDto) {
-    return 'This action adds a new order';
+  private findOrder(id: number): IOrder {
+    const order = this.orders.find((order) => order.id === id);
+    if (!order) {
+      throw new NotFoundException(`Order with id: ${id} was not found`);
+    }
+
+    return order;
   }
 
-  findAll() {
-    return `This action returns all orders`;
+  private generateNextId(): number {
+    return Math.max(...this.orders.map((product) => product.id)) + 1;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} order`;
+  private validateProductIds = (products: IProductInOrder[]) =>
+    products.forEach((product) =>
+      this.productsService.getOneById(product.productId),
+    );
+
+  private validateProductStock = (products: IProductInOrder[]) =>
+    products.forEach((product) => {
+      const productToCheck = this.productsService.getOneById(product.productId);
+      if (productToCheck.stock < product.quantity) {
+        throw new NotFoundException(
+          `Product ${productToCheck.id} is out of stock`,
+        );
+      }
+    });
+
+  create(order: CreateOrderDto): IOrder {
+    this.validateProductIds(order.products);
+    this.validateProductStock(order.products);
+
+    const newOrder: IOrder = {
+      id: this.generateNextId(),
+      madeAt: new Date(),
+      status: 'OPENED',
+      totalPrice: order.products.reduce((acc, currValue) => {
+        const currValueProduct = this.productsService.getOneById(
+          currValue.productId,
+        );
+        const currValueProductPrice =
+          currValueProduct.price * currValue.quantity;
+        return acc + currValueProductPrice;
+      }, 0),
+      ...order,
+    };
+
+    order.products.forEach((product) => {
+      const productToUpdate = this.productsService.getOneById(
+        product.productId,
+      );
+      this.productsService.update(product.productId, {
+        ...productToUpdate,
+        stock: productToUpdate.stock - product.quantity,
+      });
+    });
+
+    this.orders.push(newOrder);
+    return newOrder;
   }
 
-  update(id: number, updateOrderDto: UpdateOrderDto) {
-    return `This action updates a #${id} order`;
+  getAll(): readonly IOrder[] {
+    return this.orders;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} order`;
+  getOneById(id: number): IOrder {
+    return this.findOrder(id);
+  }
+
+  update(id: number, partialOrder: UpdateOrderDto): IOrder {
+    const orderToUpdate = this.findOrder(id);
+    Object.assign(orderToUpdate, partialOrder);
+
+    return orderToUpdate;
+  }
+
+  removeById(id: number): { id: number; removed: boolean } {
+    this.findOrder(id);
+    this.orders = this.orders.filter((order) => order.id !== id);
+
+    return { id, removed: true };
   }
 }
