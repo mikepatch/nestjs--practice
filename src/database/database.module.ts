9136430c5d07c3668e62874c5092d470e1d2abb9
Michal Lata <michallata.dev@gmail.com>
2023-12-06 10:41:32 +0100

    feat: connect Objection to Database Module and add env variable

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,16 +1,20 @@
 import { Global, Logger, Module } from '@nestjs/common';
 import knex from 'knex';
 import knexConfig from '../../knexfile';
+import { Model } from 'objection';
+import * as process from 'process';
 
 const logger = new Logger('DbConnection');
 
 const knexProvider = {
   provide: 'DbConnection',
   useFactory: async (): Promise<knex.Knex> => {
-    const connection = knex(knexConfig['development']);
+    const env = process.env.NODE_ENV || 'development';
+    const connection = knex(knexConfig[env]);
 
-    logger.log('Knex connected');
+    logger.log(`Knex connected on environment: ${env}`);
     // logger.debug(knexConfig['development']);
+    Model.knex(connection);
     return connection;
   },
 };
