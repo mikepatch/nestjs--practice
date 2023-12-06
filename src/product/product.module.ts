2d11bedae359b1cf08826d5cb59ddaa0fb01469e
Michal Lata <michallata.dev@gmail.com>
2023-12-06 14:27:51 +0100

    feat: add ProductModel and CategoryModel as providers in product module

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -3,10 +3,17 @@ import { CategoriesController } from './categories/categories.controller';
 import { ProductsController } from './products/products.controller';
 import { ProductsService } from './products/products.service';
 import { CategoriesService } from './categories/categories.service';
+import { ProductModel } from './products/product.model';
+import { CategoryModel } from './categories/category.model';
 
 @Module({
   controllers: [CategoriesController, ProductsController],
-  providers: [ProductsService, CategoriesService],
+  providers: [
+    ProductsService,
+    CategoriesService,
+    { provide: 'ProductModel', useValue: ProductModel },
+    { provide: 'CategoryModel', useValue: CategoryModel },
+  ],
   exports: [ProductsService],
 })
 export class ProductModule {}
