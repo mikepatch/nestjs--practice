5c4e50a041c62e2887751e8b9e220d023c995601
Michal Lata <michallata.dev@gmail.com>
2023-12-08 15:05:54 +0100

    patch: update database module with env variables

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,27 +1,25 @@
-import { Global, Logger, Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import knex from 'knex';
 import knexConfig from '../../knexfile';
 import { Model } from 'objection';
 import * as process from 'process';
 
 const logger = new Logger('DbConnection');
-
-const knexProvider = {
-  provide: 'DbConnection',
-  useFactory: async (): Promise<knex.Knex> => {
-    const env = process.env.NODE_ENV || 'development';
-    const connection = knex(knexConfig[env]);
-
-    logger.log(`Knex connected on environment: ${env}`);
-    // logger.debug(knexConfig['development']);
-    Model.knex(connection);
-    return connection;
-  },
-};
+const env = process.env.NODE_ENV || 'development';
 
 @Module({
-  providers: [knexProvider],
-  exports: [knexProvider],
+  providers: [
+    {
+      provide: 'DbConnection',
+      useFactory: async (): Promise<knex.Knex> => {
+        const connection = knex(knexConfig[env]);
+
+        logger.log('Knex connected');
+        Model.knex(connection);
+
+        return connection;
+      },
+    },
+  ],
 })
-@Global() // Make provider public for every part of project
 export class DatabaseModule {}
