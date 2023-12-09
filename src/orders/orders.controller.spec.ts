af360f94985bac5f6db9dd9dbafa2d630dab0b70
Michal Lata <michallata.dev@gmail.com>
2023-12-09 15:39:43 +0100

    feat: fix tests for orders controller

--- src/orders/orders.controller.spec.ts
+++ src/orders/orders.controller.spec.ts
@@ -8,7 +8,7 @@ describe('OrdersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [OrdersController],
-      providers: [OrdersService],
+      providers: [{ provide: OrdersService, useValue: {} }],
     }).compile();
 
     controller = module.get<OrdersController>(OrdersController);
