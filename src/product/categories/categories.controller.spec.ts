3430632bf92e0c31315964942aebca87e1d6cea1
Michal Lata <michallata.dev@gmail.com>
2023-12-09 15:41:10 +0100

    patch: fix tests for categories controller

--- src/product/categories/categories.controller.spec.ts
+++ src/product/categories/categories.controller.spec.ts
@@ -1,22 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoriesController } from './categories.controller';
+import { CategoriesService } from './categories.service';
 
 describe('AppController', () => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  let appController: CategoriesController;
+  let controller: CategoriesController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [CategoriesController],
-      providers: [],
+      providers: [{ provide: CategoriesService, useValue: {} }],
     }).compile();
 
-    appController = app.get<CategoriesController>(CategoriesController);
+    controller = app.get<CategoriesController>(CategoriesController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      // expect(appController.getHello()).toBe('Hello World!');
-    });
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
   });
 });
