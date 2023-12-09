52a9a04dc1e13cbafd093793bcf19d9ada73e2f2
Michal Lata <michallata.dev@gmail.com>
2023-12-09 15:41:00 +0100

    patch: fix tests for categories service

--- src/product/categories/categories.service.spec.ts
+++ src/product/categories/categories.service.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoriesService } from './categories.service';
+import { CategoriesRepository } from './categories.repository';
 
 describe('CategoriesService', () => {
   let service: CategoriesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CategoriesService],
+      providers: [
+        { provide: CategoriesRepository, useValue: {} },
+        CategoriesService,
+      ],
     }).compile();
 
     service = module.get<CategoriesService>(CategoriesService);
