7ae41c8322442334bb5a5b08686c9ae3e086babc
Michal Lata <michallata.dev@gmail.com>
2023-12-09 15:39:33 +0100

    feat: fix tests for orders service

--- src/orders/orders.service.spec.ts
+++ src/orders/orders.service.spec.ts
@@ -1,12 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrdersService } from './orders.service';
+import { ProductsService } from '../product/products/products.service';
+import { OrdersRepository } from './orders.repository';
 
 describe('OrdersService', () => {
   let service: OrdersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OrdersService],
+      providers: [
+        { provide: ProductsService, useValue: {} },
+        { provide: OrdersRepository, useValue: {} },
+        OrdersService,
+      ],
     }).compile();
 
     service = module.get<OrdersService>(OrdersService);
