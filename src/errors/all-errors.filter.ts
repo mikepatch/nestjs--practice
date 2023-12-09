99e821b887098401802fd11b737834bc9e97b537
Michal Lata <michallata.dev@gmail.com>
2023-12-09 15:41:26 +0100

    patch: refactor all errors filter

--- src/errors/all-errors.filter.ts
+++ src/errors/all-errors.filter.ts
@@ -8,24 +8,44 @@ import {
 } from '@nestjs/common';
 import { Response } from 'express';
 import { SqliteError } from 'better-sqlite3';
-import * as process from 'process';
+import { ConfigService } from '@nestjs/config';
 
 @Catch(Error)
 export class AllErrorsFilter implements ExceptionFilter {
   private logger = new Logger(AllErrorsFilter.name);
 
+  constructor(private readonly configService: ConfigService<NodeJS.AppEnv>) {}
+
   private wrapInEnvelope(
     message: string,
-    exception?: HttpException,
+    exception: any,
     statusCode = 500,
     error = 'Internal Server Error',
   ) {
+    const isDevEnv =
+      this.configService.get<NodeJS.AppEnv['NODE_ENV']>('NODE_ENV') ===
+      'development';
+
     return {
       message,
       error,
       statusCode,
-      exception,
+      ...{
+        exception: isDevEnv ? exception : undefined,
+      },
     };
+  }
+
+  private isFileSystemError(exception: any): boolean {
+    return [
+      'EACCES',
+      'EEXIST',
+      'ENOENT',
+      'ENOTDIR',
+      'ENOTEMPTY',
+      'EMFILE',
+      'EISDIR',
+    ].includes(exception?.code);
   }
 
   catch(exception: any, host: ArgumentsHost) {
@@ -38,41 +58,31 @@ export class AllErrorsFilter implements ExceptionFilter {
     if (exception instanceof HttpException) {
       response.status(exception.getStatus()).json(exception.getResponse());
 
+      this.logger.debug(`(${exception.getStatus()}): ${exception.message}`);
+      this.logger.error(exception.getResponse());
       return;
     }
 
-    if (
-      [
-        'EACCES',
-        'EEXIST',
-        'ENOENT',
-        'ENOTDIR',
-        'ENOTEMPTY',
-        'EMFILE',
-        'EISDIR',
-      ].includes(exception?.code)
-    ) {
-      if (process.env.NODE_ENV === 'development') {
-        return response
-          .status(HttpStatus.INTERNAL_SERVER_ERROR)
-          .json(this.wrapInEnvelope('File i/o error (check logs)', exception));
-      }
+    if (this.isFileSystemError(exception)) {
+      this.logger.error(exception.message);
 
       return response
         .status(HttpStatus.INTERNAL_SERVER_ERROR)
-        .json(this.wrapInEnvelope('File i/o error (check logs)'));
+        .json(this.wrapInEnvelope('File i/o error (check logs)', exception));
     }
 
     if (exception instanceof SqliteError) {
-      this.logger.error(exception);
+      this.logger.error(exception.message);
 
       return response
         .status(HttpStatus.INTERNAL_SERVER_ERROR)
-        .json(this.wrapInEnvelope('DB query error'));
+        .json(this.wrapInEnvelope('DB query error', exception));
     }
 
     response
       .status(HttpStatus.INTERNAL_SERVER_ERROR)
-      .json(this.wrapInEnvelope('Unknown error'));
+      .json(this.wrapInEnvelope('Unknown error', exception));
+
+    this.logger.error(`Unknown error: ${exception.message}`);
   }
 }
